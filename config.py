7966b72378ef702b8dca9aa533ee5a2c2a355008
Kanishka Gunasinghe <kanishkapg1121@gmail.com>
2025-08-08 21:14:15 +0530

    Add Postgre SQL database and implement the incremental processing

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+import os
+
 DATA_DIR="./data/dummy" # change into 'dummy' or 'original'
+POSTGRES_CONFIG = {
+    "dbname": os.getenv("POSTGRES_DB", "slt_circulars_db"),
+    "user": os.getenv("POSTGRES_USER", "postgres"),
+    "password": os.getenv("POSTGRES_PASSWORD", "12345678"),
+    "host": os.getenv("POSTGRES_HOST", "localhost"),
+    "port": os.getenv("POSTGRES_PORT", "5432")
+}
