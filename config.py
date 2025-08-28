089a60c71b5aebaa740de2d0a810272e76bbcae6
tharUmesh <tharunumesh1235@gmail.com>
2025-08-28 21:05:24 +0530

    requirements.txt & config.py updated

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATA_DIR="./data/dummy" # change into 'dummy' or 'original'
 POSTGRES_CONFIG = {
-    "dbname": os.getenv("POSTGRES_DB", "slt_circulars_db"),
-    "user": os.getenv("POSTGRES_USER", "postgres"),
-    "password": os.getenv("POSTGRES_PASSWORD", "12345678"),
-    "host": os.getenv("POSTGRES_HOST", "localhost"),
-    "port": os.getenv("POSTGRES_PORT", "5432")
+    "dbname": os.getenv("POSTGRES_DB"),
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+    "host": os.getenv("POSTGRES_HOST"),
+    "port": os.getenv("POSTGRES_PORT")
 }
